aa83eaeafa15a5676d0a10809d8c0d4ab86cc107
Caroline Nolasco <cnolascoix@gmail.com>
2020-07-13 11:28:14 -0700

    remove Main component and replace with main tag in Layout

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,16 +1,15 @@
 import Header from './header';
-import Main from './main';
 import style from './layout.module.scss';
 
 
 export default function Layout({ children }) {
     return(
         <div className="container">
-        <h1>Layout Component!</h1>
-        <Header />
-        <Main>
-            {children}
-        </Main>
+            <h1>Layout Component!</h1>
+            <Header />
+            <main>
+                {children}
+            </main>
         </div>
     )
 }
