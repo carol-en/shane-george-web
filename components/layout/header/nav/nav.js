5868b6ede8b4b3d6f5f41682607e8c72905bcdba
Caroline Nolasco <cnolascoix@gmail.com>
2020-07-15 09:53:49 -0700

    wip transitions for click on links

--- components/layout/header/nav/nav.js
+++ components/layout/header/nav/nav.js
@@ -1,9 +1,26 @@
 import style from './nav.module.scss';
 
+const testFunc = (event) => {
+    // console.log('Button has been clicked!');
+    const slug = event.target.id;
+    // window.location = slug;
+    setTimeout(() => { window.location = slug; }, 2000);
+}
+
+//  function or conditionals for css classes?
+
 export default function Nav() {
     return (
         <nav className={style.navi}>
-            <h1>Nav Component!</h1>
+            <button className={style.btn} onClick={testFunc} id="/">
+                Test Return
+            </button>
+            <button className={[style.btn,]} onClick={testFunc} id="about">
+                Test About
+            </button>
+            <button className={style.btn} onClick={testFunc} id="contact">
+                Test Contact
+            </button>
         </nav>
     )
 }
