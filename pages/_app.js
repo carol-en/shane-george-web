8f55af5f01b04ae58d130a3dab64e366bbd418fc
Caroline Nolasco <cnolascoix@gmail.com>
2020-08-05 10:34:40 -0700

    successfully moved contentful data to app

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,16 @@ import '../global.scss';
 import Layout from '../components/layout';
 import App from 'next/app';
 import fetch from 'isomorphic-unfetch';
+const { createClient } = require("contentful");
+
+const contentfulClient = createClient({
+  space: "6jizbkvr12of",
+  accessToken: "L60lNNHrqGGj8Y3zXd-cnI58eWKKwwcSCxyt1Z9Pm2Q"
+})
+
 
 function MyApp({ Component, pageProps, data }) {
+  
     return (
       <Layout data={data}>
         <Component {...pageProps} {...data} />
@@ -18,7 +26,20 @@ function MyApp({ Component, pageProps, data }) {
     const pages = await fetch('https://jsonplaceholder.typicode.com/posts');
     const entry = await pages.json();
     const artWork = await photos.json();
-    const data = await {entry, artWork}
+
+    const artEntries = await contentfulClient.getEntries(({
+      "content_type": "artWork",
+      "order":"sys.createdAt"
+      }));
+
+      const pageEntries = await contentfulClient.getEntries(({
+        "content_type": "page",
+        "order":"sys.createdAt"
+        }));
+    const artPieces = await artEntries.items;
+    const pgEntries = await pageEntries.items;
+    const data = await {entry, artWork, artPieces, pgEntries }
+
     return { ...appProps, data }
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
 import AllArt from '../components/portfolio/allArt';
 import { useEffect, useState } from 'react';
-import fetch from 'isomorphic-unfetch';
 const { createClient } = require("contentful");
 
-const contentfulClient = createClient({
-        space: "6jizbkvr12of",
-        accessToken: "L60lNNHrqGGj8Y3zXd-cnI58eWKKwwcSCxyt1Z9Pm2Q"
-  })
+// const contentfulClient = createClient({
+//         space: "6jizbkvr12of",
+//         accessToken: "L60lNNHrqGGj8Y3zXd-cnI58eWKKwwcSCxyt1Z9Pm2Q"
+//   })
   
 
 const IndexPage = (props) => {
 
-    const getEntry = async (entryId) => {
-        const entries = await contentfulClient.getEntries();
-        if (entries.items) return entries.items;
-        console.log(`Error getting Entries for ${contentType.name}.`)
-      }
+    // const getEntry = async () => {
+    //     const entries = await contentfulClient.getEntries();
+
+    //     if (entries.items) return entries;
+    //     console.log(`Error getting Entries for ${contentType.name}.`)
+    //   }
+
       
-      console.log(getEntry());
+      // console.log(getEntry());
+
+      console.log(props.artPieces)
+      console.log(props.pgEntries)
 
     const art = props.artWork;
     return <> <AllArt art={art} /> </>
