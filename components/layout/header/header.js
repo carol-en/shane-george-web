cb9a54837113400c7c66ba59f7647162b005fd9b
carol-en <cnolascoix@gmail.com>
2020-08-30 00:06:27 -0700

    refactor togglenav

--- components/layout/header/header.js
+++ components/layout/header/header.js
@@ -13,24 +13,21 @@ const ToggleNav = ({ art, tags }) => {
   
     // Determine what page you're on
     const checkIfInPages = (pathname, query) => {
-       let { page, param } = query;
-       let nav;
-        if(pathname !== '/' && page) { // If you're not on the home page, but on the [...page] component. Aka show page.
-            slug = page[page.length-1];
-            let artId = page[page.length-2];
-            let checkArtwork = art[artId];
-
-            if(checkArtwork) { // Check if url has necessary data to pull artwork api & that it exists/matches. If it does, do not show nav.
-                art.map(item => {
-                    let { id } = item.sys;
+        let { page, param } = query;
+        let nav = <Nav tags={tags}/>;
+            if(pathname !== '/' && page) { // If you're not on the home page, but on the [...page] component. Aka show page.
+                slug = page[page.length-1];
+                let artId = page[page.length-2];
+                let checkArtwork = art[artId];
+    
+                if(checkArtwork) { // Check if url has necessary data to pull artwork api & that it exists/matches. If it does, do not show nav.
+                    let { id } = checkArtwork.sys;
                     if(slug === id) nav = null;
-                });
-            } else nav = <Nav tags={tags}/> // if you cant pull art data, show nav
-        } 
-        else if(param) nav = null; // If you're on the  [...param] component don't show nav. Aka about/contact pages
-        else nav = <Nav tags={tags}/> // If you're on home page or anywhere else, show nav.
-        return nav;
-    }
+                } else return nav  // if you cant pull art data, show nav
+            } 
+            else if(param) nav = null; // If you're on the  [...param] component don't show nav. Aka about/contact pages
+            else return nav;
+        }
     
     const toggleNav = checkIfInPages(pathname, query);
 
